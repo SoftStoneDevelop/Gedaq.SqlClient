f4b413a00f75db681914d1e070dc6d11886b67ef
Vyacheslav <brevnov_vs.nw@uraltexis.ru>
2023-02-21 14:29:39 +0300

    init commit

---
+++ Src/Gedaq.SqlClient/Attributes/ParametrAttribute.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlTypes;
+
+namespace Gedaq.Npgsql.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Method, AllowMultiple = true)]
+    public sealed class ParametrAttribute : Attribute
+    {
+        public ParametrAttribute(
+            string methodName,
+            Type parametrType,
+            string parametrName = null,
+            SqlCompareOptions compareInfo = SqlCompareOptions.None,
+            bool forceColumnEncryption = false,
+            int localeId = 0,
+            int offset = 0,
+            SqlDbType sqlDbType = SqlDbType.NVarChar,
+            string typeName = "",
+            string udtTypeName = "",
+            string xmlSchemaCollectionDatabase = "",
+            string xmlSchemaCollectionName = "",
+            string xmlSchemaCollectionOwningSchema = "",
+            int size = -1,
+            bool nullable = false,
+            ParameterDirection direction = ParameterDirection.Input,
+            int position = -1,
+            string sourceColumn = "",
+            bool sourceColumnNullMapping = false,
+            DataRowVersion sourceVersion = DataRowVersion.Current,
+            byte scale = 0,
+            byte precision = 0
+            )
+        {
+        }
+    }
+}
---
+++ Src/Gedaq.SqlClient/Attributes/QueryAttribute.cs
@@ -0,0 +1,20 @@
+﻿using Gedaq.Common.Enums;
+using System;
+
+namespace Gedaq.Npgsql.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Method, AllowMultiple = true)]
+    public sealed class QueryAttribute : Attribute
+    {
+        public QueryAttribute(
+            string query,
+            string methodName,
+            Type queryMapType = null,
+            MethodType methodType = MethodType.Sync,
+            QueryType queryType = QueryType.Read,
+            bool generate = true
+            )
+        {
+        }
+    }
+}
