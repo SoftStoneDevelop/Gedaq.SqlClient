b5c67a3e21ff13d7947d0a7967e659509ed3812a
softstonedevelop <softstonedevelop@gmail.com>
2023-10-08 17:21:25 +0300

    feat:
    + Add ReturnType parameter
    
    https://github.com/SoftStoneDevelop/Gedaq/issues/53

--- Src/Gedaq.SqlClient/Attributes/QueryAttribute.cs
+++ Src/Gedaq.SqlClient/Attributes/QueryAttribute.cs
@@ -15,7 +15,8 @@ namespace Gedaq.SqlClient.Attributes
             bool generate = true,
             AccessModifier accessModifier = AccessModifier.AsContainingClass,
             AsyncResult asyncResultType = AsyncResult.ValueTask,
-            Type asPartInterface = null
+            Type asPartInterface = null,
+            ReturnType returnType = ReturnType.Enumerable
             )
         {
         }
