6cb9008794e3e375f63153df78b13bb7f2456549
Vyacheslav Brevnov <softstonedevelop@gmail.com>
2026-01-17 20:19:26 +0300

    feat: + Dynamic query
    #https://github.com/SoftStoneDevelop/Gedaq/issues/57
    #https://github.com/SoftStoneDevelop/Gedaq/issues/58

---
+++ Src/Gedaq.SqlClient/Attributes/DynamicParametrAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Gedaq.SqlClient.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Method, AllowMultiple = true)]
+    public sealed class DynamicParametrAttribute : Attribute
+    {
+        public DynamicParametrAttribute()
+        {
+        }
+    }
+}
--- Src/Gedaq.SqlClient/Attributes/QueryAttribute.cs
+++ Src/Gedaq.SqlClient/Attributes/QueryAttribute.cs
@@ -7,8 +7,8 @@ namespace Gedaq.SqlClient.Attributes
     public sealed class QueryAttribute : Attribute
     {
         public QueryAttribute(
-            string query,
             string methodName,
+            string query = null,
             Type queryMapType = null,
             MethodType methodType = MethodType.Sync,
             QueryType queryType = QueryType.Read,
@@ -16,8 +16,7 @@ namespace Gedaq.SqlClient.Attributes
             AccessModifier accessModifier = AccessModifier.AsContainingClass,
             AsyncResult asyncResultType = AsyncResult.ValueTask,
             Type asPartInterface = null,
-            ReturnType returnType = ReturnType.Enumerable
-            )
+            ReturnType returnType = ReturnType.List)
         {
         }
     }
