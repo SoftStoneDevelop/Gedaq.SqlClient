08129ea56525942fe516eebf9c4747daeb71b55d
Vyacheslav Brevnov <softstonedevelop@gmail.com>
2026-04-27 15:27:57 +0300

    feat: + Changes for support Gedaq 10.0.0.0

--- Src/Gedaq.SqlClient/Attributes/DynamicParametrAttribute.cs
+++ Src/Gedaq.SqlClient/Attributes/DynamicParametrAttribute.cs
@@ -2,11 +2,12 @@
 
 namespace Gedaq.SqlClient.Attributes
 {
+    /// <summary>
+    /// An attribute indicating that the parameters in a method are passed dynamically and are unknown at build time.
+    /// </summary>
+    /// <seealso href="https://github.com/SoftStoneDevelop/Gedaq.SqlClient/blob/main/Documentation/DynamicParametr.md"/>
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Method, AllowMultiple = true)]
     public sealed class DynamicParametrAttribute : Attribute
     {
-        public DynamicParametrAttribute()
-        {
-        }
     }
 }
--- Src/Gedaq.SqlClient/Attributes/ParametrAttribute.cs
+++ Src/Gedaq.SqlClient/Attributes/ParametrAttribute.cs
@@ -4,17 +4,45 @@ using System.Data.SqlTypes;
 
 namespace Gedaq.SqlClient.Attributes
 {
+    /// <summary>
+    /// An attribute for specifying a parameter passed in a request.
+    /// </summary>
+    /// <seealso href="https://github.com/SoftStoneDevelop/Gedaq.SqlClient/blob/main/Documentation/Parametr.md"/>
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Method, AllowMultiple = true)]
     public sealed class ParametrAttribute : Attribute
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="parametrType">The type of parameter added to the generated query</param>
+        /// <param name="parametrName">Parameter name if it is a named parameter, there are also positional parameters <paramref name="position"/></param>
+        /// <param name="compareInfo"></param>
+        /// <param name="forceColumnEncryption"></param>
+        /// <param name="localeId"></param>
+        /// <param name="offset"></param>
+        /// <param name="dbType"></param>
+        /// <param name="typeName"></param>
+        /// <param name="udtTypeName"></param>
+        /// <param name="xmlSchemaCollectionDatabase"></param>
+        /// <param name="xmlSchemaCollectionName"></param>
+        /// <param name="xmlSchemaCollectionOwningSchema"></param>
+        /// <param name="size"></param>
+        /// <param name="nullable"></param>
+        /// <param name="direction"></param>
+        /// <param name="sourceColumn"></param>
+        /// <param name="sourceColumnNullMapping"></param>
+        /// <param name="sourceVersion"></param>
+        /// <param name="scale"></param>
+        /// <param name="precision"></param>
+        /// <param name="methodParametrName">The name of the parameter in the generated method. If not specified, an abstract name will be used, such as parameter1, parameter2, etc.</param>
         public ParametrAttribute(
             Type parametrType,
-            string parametrName = null,
+            string? parametrName = null,
             SqlCompareOptions compareInfo = SqlCompareOptions.None,
             bool forceColumnEncryption = false,
             int localeId = 0,
             int offset = 0,
-            SqlDbType sqlDbType = SqlDbType.NVarChar,
+            SqlDbType dbType = SqlDbType.NVarChar,
             string typeName = "",
             string udtTypeName = "",
             string xmlSchemaCollectionDatabase = "",
@@ -28,8 +56,7 @@ namespace Gedaq.SqlClient.Attributes
             DataRowVersion sourceVersion = DataRowVersion.Current,
             byte scale = 0,
             byte precision = 0,
-            string methodParametrName = null
-            )
+            string? methodParametrName = null)
         {
         }
     }
